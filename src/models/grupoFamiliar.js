42aec557f6efbaa87062ea7ae59f7a4dc7453a8d
Mauropestarino <pestarino35@gmail.com>
2024-04-23 17:58:02 -0300

    creando proyecto

---
+++ src/models/grupoFamiliar.js
@@ -0,0 +1,31 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(
+  ""
+);
+
+// Definición del modelo GrupoFamiliar
+const GrupoFamiliar = sequelize.define(
+  "GrupoFamiliar",
+  {
+    grupoFamiliarId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    personaId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: "Persona", // Nombre de la tabla referenciada
+        key: "personaId", // Clave primaria de la tabla referenciada
+      },
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "grupos_familiares",
+  }
+);
+
+module.exports = GrupoFamiliar;
---
+++ src/models/persona.js
@@ -0,0 +1,40 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  ""
+);
+
+// Definición del modelo Persona
+const Persona = sequelize.define('Persona', {
+  personaId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    unique: true
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  apellido: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  edad: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  nacionalidad: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  restriccionesId: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: false,
+  tableName: 'personas'
+});
+
+module.exports = Persona;
