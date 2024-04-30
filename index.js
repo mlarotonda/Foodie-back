07741c2d185265ae5d474e02c208434e9549542d
mlarotonda <mauricio.larotonda@fravega.com.ar>
2024-04-29 21:03:50 -0300

    index

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import router from "./src/routes/router.js";
+import connection from "./src/connection/connection.js";
+import { config } from "./src/config/config.js";
+import cors from "cors"
+
+const app = express();
+app.use(cors({origin: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use("/api", router);
+app.use((req,res) => {
+	res.status(400).send('EndPoint Not Found')
+})
+await connection.sync({ force: false });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.listen(config.serverPort, () => {
+    console.log("El servidor esta funcionando en el puerto " + config.serverPort)
+});
