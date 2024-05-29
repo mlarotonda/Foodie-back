44e7190324b3f2586718f1109da5f412805ecc03
Mauropestarino <pestarino35@gmail.com>
2024-05-29 19:58:05 -0300

    implementacion firestore y carga tipoProductos en bd

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,25 @@
 import "dotenv/config";
 
+const apiKeyFirebase = process.env.API_KEY_FIREBASE;
+const authDomain = process.env.AUTH_DOMAIN;
+const projetctId = process.env.PROJECT_ID;
+const storageBucket = process.env.STORAGE_BUCKET;
+const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+const appId = process.env.APP_ID;
 const serverPort = process.env.SERVER_PORT;
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbDialect = process.env.DB_DIALECT;
-const dbPort = process.env.DB_PORT;
 const apiKeyGemini = process.env.API_KEY_GEMINI;
 const linkMongoDb = process.env.LINK_MONGO;
+const measurementId = process.env.MEASURENT_ID;
 
 const config = {
+  apiKeyFirebase,
+  authDomain,
+  projetctId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
   serverPort,
-  dbName,
-  dbUser,
-  dbPassword,
-  dbHost,
-  dbDialect,
-  dbPort,
   apiKeyGemini,
   linkMongoDb
 };
--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,14 +1,28 @@
-import mongoose from "mongoose";
-import {config} from "../config/config.js"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { config } from "../config/config.js";
+import { getFirestore } from "firebase/firestore";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(config.linkMongoDb);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: config.apiKeyFirebase,
+  authDomain: config.authDomain,
+  projectId: config.projetctId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 
-export default connectDB;
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+
+//const analytics = getAnalytics(app);
+
+export {firebase, db};
