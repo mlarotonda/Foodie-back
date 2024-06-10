e320863ecafc4b06df2899dd858fb238dbd440b1
Mauropestarino <pestarino35@gmail.com>
2024-06-10 17:06:23 -0300

    grupofamiliar controller, y routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,11 +3,14 @@ import userRoutes from "./userRoutes.js";
 import ratoneandoRoutes from "./ratoneandoRoutes.js";
 import personaRoutes from "./personaRoutes.js"
 import stockRoutes from "./stockRoutes.js"
+import grupoFamiliarRoutes from "../routes/grupoFamiliarRoutes.js";
+
 const router = Router();
 
 router.use("/usuarios", userRoutes);
 router.use("/ratoneando", ratoneandoRoutes);
 router.use("/personas", personaRoutes);
 router.use("/stock", stockRoutes);
+router.use("/grupoFamiliar", grupoFamiliarRoutes);
 
 export default router;
