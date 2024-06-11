19ba92b41516022d592a32648931ed7a57057f50
mlarotonda <mauricio.larotonda@fravega.com.ar>
2024-06-10 22:04:27 -0300

    GeminiController

--- src/controller/GeminiController.js
+++ src/controller/GeminiController.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = "ya29.a0AXooCgs9L-ujO3Lm852UVlaKwVNWOR4aHgkULO9WrcSIaL4plIk33AWTxyOlchfHq8gCLZergeS-sJ-henqVW6vOfYIBy6wWJ1o59iZW6vAofYX4tJFxO7Z-MfitUVtCw9jXX38MkRKJerJN4BFG_ab7DoBk52cmLSuOqlpgg875ZXeqSydAJ8-iZE0_ByMga0jg2J7ogDhsSq88kCZcbRPWWiY9vP9Rm5mfzd8b93I9i_QU8RSkCl7KhI2wmgmPd8HQharLFhYJc0J8GiUepWQ8hmwNjtg9s5OPufIEPDoFwiLnBFXnpL3qk7iMRF1vm8RsqmMmWlgYzctqCxUt0NyWSefZgONzk6mubGxZxfzaE2DwrZmbVqBDM4rndrg1mQyKd1vhEhlkNZZlRm_BAAlCQN04-CsqaCgYKAQMSARISFQHGX2MiM_fnJeC5IA5u70SAKxkIUg0423"
+const token = "ya29.a0AXooCgtzjKgpJFzbGRierc2rjbFAiYjuWDukYXd0kN2tgKcgNFKQssxBZliCGZ2eNpFfz-elC4jDCuV5lo2u8ywL8MAF63FEv9kbBs9iPMf2pccsVy6rOeqp4B-Vz8vGG36xgBtI_ePetqLMgw48Ye4bGfBPcJAJdxQVndgttC21ixY6N-dq0sAqz6Zem_fZqFGjz4D9tTdWYxMg7S5oo8AST9ILLrYdpUJECY6jKsf11ZGoG6PevA7WQ4JLVeqGS_-ARVMX7dNZpH511QEQ_bI6u0LMIELuM0wgp7UtI7jZP7kz7oxAGDD2jVdA5bcU-spxhliKnLNFyiFQ5Goy8Xrjx-s0NFaYR8gql9LEF8EIQqzYI4vZsMsy1Lf3hBB3vVAGV5BWBHcvsfFwHtgSlZxpk78oWhBeaCgYKAYwSARISFQHGX2Mi17t1t1MdnCLiqqeyMK9AwQ0423"
 const url = "https://us-central1-aiplatform.googleapis.com/v1/projects/foodie-1614e/locations/us-central1/publishers/google/models/gemini-1.5-flash-001:streamGenerateContent"
 const ingredientes = "pollo, zanahoria y cebolla"
 
@@ -34,6 +34,9 @@ class GeminiController {
                   {
                       "text": "- Las recetas deben tener ingredientes y pasos"
                   },
+                  //{
+                  //    "text": "- Incluir una imagen para cada receta"
+                  //},
                   {
                       "text": "- Los ingredientes deben estar expresados en gramos y mililitros"
                   },
